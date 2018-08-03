6f174193e9956068c06a13da1f96b2cde9290071
markovirtanen <41877210+markovirtanen@users.noreply.github.com>
2018-08-03 07:37:51 +0100

    minor changes

--- src/PoolWallet/PoolWallet.cpp
+++ src/PoolWallet/PoolWallet.cpp
@@ -1620,13 +1620,13 @@ int main(int argc, char* argv[]) {
 
   logManager.configure(buildLoggerConfiguration(logLevel, Common::ReplaceExtenstion(argv[0], ".log")));
 
-  std::cout << "TurtleCoin v" << PROJECT_VERSION << " Poolwallet" << std::endl;
+  std::cout << "Petcoin v" << PROJECT_VERSION << " Poolwallet" << std::endl;
 
   std::cout << "Please note that usage of simplewallet/poolwallet has been "
             << "deprecated for pool usage." << std::endl
-            << "If you are using turtle-pool, you can trivially transfer to "
-            << "walletd by following these instructions:" << std::endl
-            << "https://github.com/turtlecoin/turtle-pool/pull/5" << std::endl;
+            << ""
+            << "" << std::endl
+            << "" << std::endl;
 
   CryptoNote::Currency currency = CryptoNote::CurrencyBuilder(logManager).
     testnet(command_line::get_arg(vm, arg_testnet)).currency();
